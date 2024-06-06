b177fb91adf5724ca99a1fc977ee7e95919f0767
AbdulrahmanA81 <70779417+AbdulrahmanA81@users.noreply.github.com>
2024-06-05 23:25:58 -0400

    Update kernel1.cu

--- kernel1.cu
+++ kernel1.cu
@@ -1,17 +1,3 @@
-/* ------------
- * This code is provided solely for the personal and private use of
- * students taking the CSC367H5 course at the University of Toronto.
- * Copying for purposes other than this use is expressly prohibited.
- * All forms of distribution of this code, whether as given or with
- * any changes, are expressly prohibited.
- *
- * Authors: Bogdan Simion, Felipe de Azevedo Piovezan
- *
- * All of the files in this directory and all subdirectories are:
- * Copyright (c) 2022 Bogdan Simion
- * -------------
- */
-
 #include "kernels.h"
 #include <iostream>
 
